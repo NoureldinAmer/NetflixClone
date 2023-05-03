753fe1c84d34ad66cc9507a51c35efcf95f8acef
Nour Amer <84950398+NoureldinAmer@users.noreply.github.com>
2023-05-03 01:02:20 -0600

    setup main app navigatiom

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import * as React from "react";
 import { StyleSheet, View } from "react-native";
 import Navigation from "./screens/Navigation";
+import { useFonts } from "expo-font";
+import AppLoading from "expo-app-loading";
 
 const App = () => {
+  const [fontsLoaded] = useFonts({
+    "netflix-bold": require("./assets/fonts/NetflixSans-Bold.otf"),
+    "netflix-light": require("./assets/fonts/NetflixSans-Light.otf"),
+    "netflix-medium": require("./assets/fonts/NetflixSans-Medium.otf"),
+    "netflix-regular": require("./assets/fonts/NetflixSans-Regular.otf"),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <View style={styles.container}>
       <Navigation />
